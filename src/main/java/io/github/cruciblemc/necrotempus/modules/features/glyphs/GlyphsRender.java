2ddd38f0d63c7bb44931eb126ba4cfe3355ad97c
Bruno Marques Caroba <bruno_caroba@hotmail.com>
2025-03-14 12:58:56 -0300

    [ci skip]Removed try catch to use SneakyThrows; this system should not have any exceptions. Fixed Z level on certain contexts with modern fonts.

--- src/main/java/io/github/cruciblemc/necrotempus/modules/features/glyphs/GlyphsRender.java
+++ src/main/java/io/github/cruciblemc/necrotempus/modules/features/glyphs/GlyphsRender.java
@@ -2,64 +2,66 @@ package io.github.cruciblemc.necrotempus.modules.features.glyphs;
 
 import io.github.cruciblemc.necrotempus.modules.features.modernfonts.ModernFontEntry;
 import io.github.cruciblemc.necrotempus.utils.MathUtils;
+import lombok.SneakyThrows;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.texture.TextureManager;
 import org.lwjgl.opengl.GL11;
 
 public class GlyphsRender {
 
+    @SneakyThrows
     public static float renderGlyph(TextureManager textureManager, ModernFontEntry entry,
                                     float posX, float posY, boolean shadow) {
 
-        try {
+        GL11.glPushMatrix();
+        GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
 
-            GL11.glPushMatrix();
+        textureManager.bindTexture(entry.location);
 
-            textureManager.bindTexture(entry.location);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
 
-            drawGlyphAtlas(posX, posY, entry, shadow);
+        drawGlyphAtlas(posX, posY, entry, shadow);
 
-            GL11.glPopMatrix();
-
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
+        GL11.glDisable(GL11.GL_DEPTH_TEST);
+        GL11.glDisable(GL11.GL_BLEND);
+        GL11.glPopAttrib();
+        GL11.glPopMatrix();
 
         return entry.width + 1;
+
     }
 
 
+    @SneakyThrows
     public static float renderGlyph(TextureManager textureManager, CustomGlyphs customGlyphs, float posX, float posY, boolean shadow, float alpha) {
 
         if (!shadow) {
-            try {
+            GL11.glPushMatrix();
+            GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
+            GL11.glColor4f(1, 1, 1, alpha);
 
-                GL11.glPushMatrix();
-                GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
-                GL11.glColor4f(1, 1, 1, alpha);
+            textureManager.bindTexture(customGlyphs.getResource());
 
-                textureManager.bindTexture(customGlyphs.getResource());
+            GL11.glEnable(GL11.GL_BLEND);
+            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+            GL11.glEnable(GL11.GL_DEPTH_TEST);
 
-                GL11.glEnable(GL11.GL_BLEND);
-                GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-                GL11.glEnable(GL11.GL_DEPTH_TEST);
+            float realX = posX + (customGlyphs.getHorizontalPadding() * -1);
+            float realY = posY + (customGlyphs.getVerticalPadding() * -1);
 
-                float realX = posX + (customGlyphs.getHorizontalPadding() * -1);
-                float realY = posY + (customGlyphs.getVerticalPadding() * -1);
+            if (customGlyphs.getFitMode() != CustomGlyphs.FitMode.NONE) {
+                drawGlyphContains(realX, realY, customGlyphs);
+            } else
+                drawGlyph(realX, realY, customGlyphs.getWidth(), customGlyphs.getHeight());
 
-                if (customGlyphs.getFitMode() != CustomGlyphs.FitMode.NONE) {
-                    drawGlyphContains(realX, realY, customGlyphs);
-                } else
-                    drawGlyph(realX, realY, customGlyphs.getWidth(), customGlyphs.getHeight());
+            GL11.glDisable(GL11.GL_DEPTH_TEST);
+            GL11.glDisable(GL11.GL_BLEND);
+            GL11.glPopAttrib();
+            GL11.glPopMatrix();
 
-                GL11.glDisable(GL11.GL_DEPTH_TEST);
-                GL11.glDisable(GL11.GL_BLEND);
-                GL11.glPopAttrib();
-                GL11.glPopMatrix();
 
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
         }
 
         return customGlyphs.getFinalCharacterWidth();
@@ -97,10 +99,10 @@ public class GlyphsRender {
 
         ts.startDrawingQuads();
 
-        add(ts, x - offset, y + entry.height, u0, v1);            // Bottom-left
-        add(ts, x + entry.width - offset, y + entry.height, u1, v1); // Bottom-right
-        add(ts, x + entry.width + offset, y, u1, v0);                // Top-right
-        add(ts, x + offset, y, u0, v0);                              // Top-left
+        addToZero(ts, x - offset, y + entry.height, u0, v1);            // Bottom-left
+        addToZero(ts, x + entry.width - offset, y + entry.height, u1, v1); // Bottom-right
+        addToZero(ts, x + entry.width + offset, y, u1, v0);                // Top-right
+        addToZero(ts, x + offset, y, u0, v0);                              // Top-left
 
         ts.draw();
 
@@ -140,5 +142,8 @@ public class GlyphsRender {
         tessellator.addVertexWithUV(x, y, 50F, textureX, textureY);
     }
 
+    private static void addToZero(Tessellator tessellator, float x, float y, float textureX, float textureY) {
+        tessellator.addVertexWithUV(x, y, 0F, textureX, textureY);
+    }
 
 }
